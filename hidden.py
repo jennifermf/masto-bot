de3b2967f31feebacc24045d448332a8ed71ba87
Malte Dik <github@enteig.net>
2017-09-05 02:20:38 +0000

    removed hidden.py, config will be moved to command line arguments, and files as suggested by Mastodon.py

--- hidden.py
+++
@@ -1,7 +0,0 @@
-def secret():
-    return {
-        'client_id': 'xxxxxxxxxxxxxxxxxxxxxxxx',
-        'client_secret': 'xxxxxxxxxxxxxxxxxxxxxxxx',
-        'access_token': 'xxxxxxxxxxxxxxxxxxxxxxxx',
-        'email': 'bot_email@email.internet.com',
-        'password': 'bot_pwd'}
